6aee1c211e18252e0dd1af535b545733680a6b0e
Corey Coleman <coreycoleman1339@gmail.com>
2021-08-04 17:50:16 -0700

    display navbar title on scroll

--- js/scipts.js
+++ js/scipts.js
@@ -48,3 +48,24 @@ function cardChecker(cardNumber, delay) {
 cardChecker(1, 0);
 cardChecker(2, 250);
 cardChecker(3, 500);
+
+
+
+window.onscroll = function() {scrollFunction()};
+
+
+
+function scrollFunction() {
+    let jumboHeight = document.getElementById("top-jumbotron").offsetHeight;
+  if (document.body.scrollTop > jumboHeight || document.documentElement.scrollTop > jumboHeight) {
+    document.getElementById("nav-title").style.fontSize = "25px";
+    document.getElementById("nav-title").style.visibility = "visible";
+    document.getElementById("nav-title").style.opacity = "1";
+  } else {
+    document.getElementById("nav-title").style.fontSize = "1px";
+    document.getElementById("nav-title").style.visibility = "hidden";
+    document.getElementById("nav-title").style.opacity = "0";
+  }
+}
+
+console.log(document.getElementById("top-jumbotron").offsetHeight);
